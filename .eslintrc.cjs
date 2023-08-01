aa08e2ff025b8d1d7ed2f9c0033f00d9ae651703
kentahayashi <kenta0810hayashi@gmail.com>
2023-08-01 21:15:33 +0200

    L3-start

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,16 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'off' }],
+  },
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
